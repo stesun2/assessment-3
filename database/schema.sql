0846f66c4f0e30b98526a032b9e482cd531f9423
stesun2 <ss27@illinois.edu>
2021-06-27 16:26:48 -0500

    updated basic schema and seed sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,39 +2,48 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 
 DROP TABLE IF EXISTS customer CASCADE;
-DROP TABLE IF EXISTS product CASCADE;
-DROP TABLE IF EXISTS category CASCADE;
-DROP TABLE IF EXISTS order-info CASCADE;
-
-
+DROP TABLE IF EXISTS addresses CASCADE;
+DROP TABLE IF EXISTS order_info CASCADE;
+DROP TABLE IF EXISTS product_info CASCADE;
+DROP TABLE IF EXISTS category_info CASCADE;
 
 CREATE TABLE customer (
     id          serial        PRIMARY KEY,
     first_name  VARCHAR(24)   NOT NULL,
     last_name   VARCHAR(24)   NOT NULL,
     email       VARCHAR(48)   NOT NULL,
-    addresses   VARCHAR(255)  NOT NULL,
+    address_id  INTEGER,      
+    order_id    INTEGER 
 );
 
-CREATE TABLE product (
+CREATE TABLE addresses (
+    id          serial        PRIMARY KEY,
+    line_1      VARCHAR(255)  NOT NULL,
+    city        VARCHAR(24)   NOT NULL,
+    state       VARCHAR(2)    NOT NULL,
+    zipcode     INTEGER       NOT NULL
+
+);
+
+CREATE TABLE order_info (
+    id           serial       PRIMARY KEY,
+    order_id     INTEGER      NOT NULL,
+    order_date   DATE         NOT NULL
+);
+
+CREATE TABLE product_info (
     id           serial       PRIMARY KEY,
     product_name VARCHAR(48)  NOT NULL,
     description  VARCHAR(48)  NOT NULL,
     product_id   INTEGER      NOT NULL,  
-    quantity     INTEGER      NOT NULL,
-    unit_price   money  
+    unit_price   MONEY        NOT NULL,
+    category_id  INTEGER
+
 );
 
-CREATE TABLE category (
+CREATE TABLE category_info (
     id           serial       PRIMARY KEY,
-    room_type    VARCHAR(24)  NOT NULL
+    category     VARCHAR(24)  NOT NULL
 );
 
-CREATE TABLE order-info (
-    id           serial       PRIMARY KEY,
-    invoice_num  INTEGER      NOT NULL,
-    quantity     INTEGER      NOT NULL,
-    order_id     INTEGER      NOT NULL,
-    order_date   INTEGER      NOT NULL
-);
 
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,21 @@
 -- Seed data goes here
+
+-- Insert Customers
+INSERT into customer (id, first_name, last_name, email, address_id, order_id)
+values(1, 'Bugs', 'Bunny', 'BugsBunny@gmail.com', 1, 1);
+
+-- Insert Addresses
+INSERT into addresses (id, line_1, city, state, zipcode)
+values(1, '123 Fake St', 'Albuquerque', 'NM', '87101');
+
+-- Insert Order Info
+INSERT into order_info (id, order_id, order_date)
+values(1, 1, '2021-06-27');
+
+-- Product Info
+INSERT into product_info (id, product_name, description, product_id, unit_price, category_id)
+values (1, 'Carrots', 'Bag of carrots', '001', 4.99, 1);
+
+-- Category Info
+INSERT into category_info (id, category)
+values (1, 'Kitchen');
